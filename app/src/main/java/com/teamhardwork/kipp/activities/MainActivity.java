3e7c474ba58a2ac914fbf849384011763505e006
Raymond Carino <rcarino@Raymonds-MacBook-Air.local>
2014-10-12 22:15:49 -0700

    really basic and crappy main activity

--- app/src/main/java/com/teamhardwork/kipp/activities/MainActivity.java
+++ app/src/main/java/com/teamhardwork/kipp/activities/MainActivity.java
@@ -1,10 +1,17 @@
 package com.teamhardwork.kipp.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.echo.holographlibrary.PieGraph;
+import com.echo.holographlibrary.PieSlice;
 import com.teamhardwork.kipp.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +21,63 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Welcome to KIPP!", Toast.LENGTH_SHORT).show();
+        setupMockRoster();
+        setupMockFeed();
+        setupMockLeaderboard();
+        setupMockStatsModule();
+    }
 
+    private void setupMockRoster() {
+        ArrayList<String> roster = new ArrayList<String>();
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, roster);
+        itemsAdapter.add("Class Roster");
+        itemsAdapter.add("Bruce Wayne");
+        itemsAdapter.add("Clark Kent");
+        itemsAdapter.add("John Doe");
+        itemsAdapter.add("Jane Doe");
+        ListView lvRoster = (ListView) findViewById(R.id.lvClassRoster);
+        lvRoster.setAdapter(itemsAdapter);
+    }
+
+    private void setupMockFeed() {
+        ArrayList<String> feed = new ArrayList<String>();
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, feed);
+        itemsAdapter.add("Feed");
+        itemsAdapter.add("John Doe making noise (1 min ago in Math - Mr. Wright)");
+        itemsAdapter.add("Jane Doe being a distraction (3 hrs ago in Eng - Mrs. Wong)");
+        itemsAdapter.add("Bruce Wayne helped others (1 day ago)\n");
+        ListView lvFeed = (ListView) findViewById(R.id.lvFeed);
+        lvFeed.setAdapter(itemsAdapter);
+    }
+
+    private void setupMockLeaderboard() {
+        ArrayList<String> leaderBoard = new ArrayList<String>();
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, leaderBoard);
+        itemsAdapter.add("Top Students:");
+        itemsAdapter.add("Bruce Wayne");
+        itemsAdapter.add("John Doe");
+        itemsAdapter.add("Worst Students:");
+        itemsAdapter.add("Jane Doe");
+        ListView lvLeaderboard = (ListView) findViewById(R.id.lvLeaderBoard);
+        lvLeaderboard.setAdapter(itemsAdapter);
+    }
+
+    private void setupMockStatsModule(){
+        PieGraph pg = (PieGraph)findViewById(R.id.pgTest);
+        PieSlice slice = new PieSlice();
+        slice.setColor(Color.parseColor("#99CC00"));
+        slice.setValue(2);
+        slice.setTitle("I'm a good boy");
+        pg.addSlice(slice);
+        slice = new PieSlice();
+        slice.setColor(Color.parseColor("#FFBB33"));
+        slice.setValue(3);
+        slice.setTitle("I'm a bad boy");
+        pg.addSlice(slice);
+        slice = new PieSlice();
+        slice.setColor(Color.parseColor("#AA66CC"));
+        slice.setValue(8);
+        slice.setTitle("Awkward silence");
+        pg.addSlice(slice);
     }
 }
