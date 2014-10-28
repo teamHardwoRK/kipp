396118fc6db277f3daabe40160ae96d899629b36
Kevin Leong <kevin.leong@threadflip.com>
2014-10-28 10:11:42 -0700

    Style leaderboard.

--- app/src/main/java/com/teamhardwork/kipp/adapters/LeaderboardAdapter.java
+++ app/src/main/java/com/teamhardwork/kipp/adapters/LeaderboardAdapter.java
@@ -1,6 +1,10 @@
 package com.teamhardwork.kipp.adapters;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,19 +13,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.teamhardwork.kipp.KippApplication;
 import com.teamhardwork.kipp.R;
 import com.teamhardwork.kipp.models.users.Student;
+import com.teamhardwork.kipp.utilities.GraphicsUtils;
 import com.teamhardwork.kipp.utilities.RandomApiUrlGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class LeaderboardAdapter extends ArrayAdapter<Student> {
+    List<Integer> colorList = new ArrayList<Integer>();
 
     public LeaderboardAdapter(Context context, List<Student> objects) {
         super(context, 0, objects);
+
+        colorList = GraphicsUtils.colorList(context);
     }
 
     @Override
@@ -34,9 +45,16 @@ public class LeaderboardAdapter extends ArrayAdapter<Student> {
             convertView = LayoutInflater.from(getContext())
                     .inflate(R.layout.item_leaderboard_entry, parent, false);
             holder = new ViewHolder(convertView);
+            Typeface defaultTypeface = KippApplication.getDefaultTypeFace(getContext());
+            holder.tvName.setTypeface(defaultTypeface);
+            holder.tvPoints.setTypeface(defaultTypeface);
+            holder.tvRank.setTypeface(defaultTypeface);
             convertView.setTag(holder);
         }
 
+        int backgroundColor = colorList.get(new Random().nextInt(colorList.size()));
+        ((GradientDrawable)holder.tvRank.getBackground()).setColor(
+                backgroundColor);
         Student student = getItem(position);
         populateViewHolder(holder, student, position + 1);
 
--- app/src/main/java/com/teamhardwork/kipp/utilities/GraphicsUtils.java
+++ app/src/main/java/com/teamhardwork/kipp/utilities/GraphicsUtils.java
@@ -1,7 +1,13 @@
 package com.teamhardwork.kipp.utilities;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
+
+import com.teamhardwork.kipp.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GraphicsUtils {
     public static int dpToPx(int dp) {
@@ -10,5 +16,18 @@ public class GraphicsUtils {
 
     public static int pxToDp(int px) {
         return Math.round(px / (Resources.getSystem().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
+    public static List<Integer> colorList(Context context) {
+        List<Integer> colorList = new ArrayList<Integer>();
+
+        Resources resources = context.getResources();
+        colorList.add(resources.getColor(R.color.DodgerBlue));
+        colorList.add(resources.getColor(R.color.ForestGreen));
+        colorList.add(resources.getColor(R.color.PumpkinOrange));
+        colorList.add(resources.getColor(R.color.Beer));
+        colorList.add(resources.getColor(R.color.PaleVioletRed));
+
+        return colorList;
     }
 }
