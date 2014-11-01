5263f291a1e790fe0a14469fd2eb2eab5df25bf0
Raymond Carino <rc@berkeley.edu>
2014-10-31 18:18:33 -0700

    action bar search

--- app/src/main/java/com/teamhardwork/kipp/adapters/StudentArrayAdapter.java
+++ app/src/main/java/com/teamhardwork/kipp/adapters/StudentArrayAdapter.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,20 +24,35 @@ import com.teamhardwork.kipp.models.users.Student;
 import com.teamhardwork.kipp.queries.FeedQueries;
 import com.teamhardwork.kipp.utilities.Recommendation;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class StudentArrayAdapter extends ArrayAdapter<Student> {
-    private Context context;
+public class StudentArrayAdapter extends ArrayAdapter<Student> implements Filterable {
     public static final int warningColor = Color.parseColor("#FFD119"); // color for warning tips
     public static final int infoColor = Color.parseColor("#1C70EF");
+    private Context context;
+    private List<Student> students;
+    private List<Student> originalRoster;
+    private Filter studentFilter;
 
     public StudentArrayAdapter(Context context, int resource, List<Student> students) {
         super(context, resource, students);
         this.context = context;
+        this.students = students;
+        this.originalRoster = new ArrayList<Student>(students);
+    }
+
+    public static String getBehaviorHtmlString(Behavior behavior) {
+        if (behavior.getCategory() == BehaviorCategory.SLIP ||
+                behavior.getCategory() == BehaviorCategory.FALL) {
+            return new String("<font color=\"#FF6B6B\"> - </font>");
+        } else {
+            return new String("<font color=\"#C7F464\"> + </font>");
+        }
     }
 
     @Override
@@ -86,31 +103,28 @@ public class StudentArrayAdapter extends ArrayAdapter<Student> {
 
                 if (Recommendation.getInstance().hasRecs(student)) {
                     holder.ivTips.setImageResource(R.drawable.ic_tips);
-                    int tipColor = (Recommendation.getInstance().getRecs(student).getRecType() == Recommendation.RecommendationType.BAD) ?
+                    int tipColor = (Recommendation.getInstance().getRecs(student).getRecType()
+                            == Recommendation.RecommendationType.BAD) ?
                             warningColor : infoColor;
                     holder.ivTips.setColorFilter(tipColor);
                 }
 
-                for(int i = behaviorEvents.size() - 1; i >= 0; i--) {
+                for (int i = behaviorEvents.size() - 1; i >= 0; i--) {
                     int eventResource = behaviorEvents.get(i).getBehavior().getColorResource();
 
-                    if(i == behaviorEvents.size() - 1) {
+                    if (i == behaviorEvents.size() - 1) {
                         holder.ivLastBehavior.setImageResource(eventResource);
                         holder.ivLastBehavior.setVisibility(View.VISIBLE);
-                    }
-                    else if(i == behaviorEvents.size() - 2) {
+                    } else if (i == behaviorEvents.size() - 2) {
                         holder.ivFourthBehavior.setImageResource(eventResource);
                         holder.ivFourthBehavior.setVisibility(View.VISIBLE);
-                    }
-                    else if(i == behaviorEvents.size() - 3) {
+                    } else if (i == behaviorEvents.size() - 3) {
                         holder.ivThirdBehavior.setImageResource(eventResource);
                         holder.ivThirdBehavior.setVisibility(View.VISIBLE);
-                    }
-                    else if(i == behaviorEvents.size() - 4) {
+                    } else if (i == behaviorEvents.size() - 4) {
                         holder.ivSecondBehavior.setImageResource(eventResource);
                         holder.ivSecondBehavior.setVisibility(View.VISIBLE);
-                    }
-                    else if(i == behaviorEvents.size() - 5) {
+                    } else if (i == behaviorEvents.size() - 5) {
                         holder.ivFirstBehavior.setImageResource(eventResource);
                         holder.ivFirstBehavior.setVisibility(View.VISIBLE);
                     }
@@ -119,37 +133,65 @@ public class StudentArrayAdapter extends ArrayAdapter<Student> {
         });
     }
 
-    public static String getBehaviorHtmlString(Behavior behavior) {
-        if (behavior.getCategory() == BehaviorCategory.SLIP ||
-                behavior.getCategory() == BehaviorCategory.FALL) {
-            return new String("<font color=\"#FF6B6B\"> - </font>");
-        } else {
-            return new String("<font color=\"#C7F464\"> + </font>");
+    public void resetFilter() {
+        clear();
+        addAll(originalRoster);
+    }
+
+    public Filter getFilter() {
+        if (studentFilter == null) {
+            studentFilter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults results = new FilterResults();
+                    List<Student> filteredStudents = new ArrayList<Student>();
+
+                    for (Student student : originalRoster) {
+                        if (student.getFirstName().toLowerCase().startsWith(constraint.toString()))
+                            filteredStudents.add(student);
+                    }
+                    results.values = filteredStudents;
+                    results.count = filteredStudents.size();
+                    return results;
+                }
+
+                @SuppressWarnings("unchecked")
+                @Override
+                protected void publishResults(CharSequence constraint,
+                                              FilterResults results) {
+                    // Now we have to inform the adapter about the new list filtered
+                    if (results.count == 0)
+                        notifyDataSetInvalidated();
+                    else {
+                        clear();
+                        addAll((List<Student>) results.values);
+                    }
+                }
+            };
         }
+
+        return studentFilter;
+    }
+
+    public void setOriginalRoster(List<Student> originalRoster) {
+        this.originalRoster = originalRoster;
     }
 
     static class ViewHolder {
         @InjectView(R.id.ivProfilePic)
         ImageView ivProfilePic;
-
         @InjectView(R.id.tvName)
         TextView tvName;
-
         @InjectView(R.id.ivFirstBehavior)
         ImageView ivFirstBehavior;
-
         @InjectView(R.id.ivSecondBehavior)
         ImageView ivSecondBehavior;
-
         @InjectView(R.id.ivThirdBehavior)
         ImageView ivThirdBehavior;
-
         @InjectView(R.id.ivFourthBehavior)
         ImageView ivFourthBehavior;
-
         @InjectView(R.id.ivLastBehavior)
         ImageView ivLastBehavior;
-
         @InjectView(R.id.ivTips)
         ImageView ivTips;
 
