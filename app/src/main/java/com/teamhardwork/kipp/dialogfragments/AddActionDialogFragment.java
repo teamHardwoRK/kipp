f053c8cdcba3dc7ccc33f9fe47dfa3c09c8cf996
Raymond Carino <rc@berkeley.edu>
2014-11-07 19:18:19 -0800

    add rebound click animation to action logging

--- app/src/main/java/com/teamhardwork/kipp/dialogfragments/AddActionDialogFragment.java
+++ app/src/main/java/com/teamhardwork/kipp/dialogfragments/AddActionDialogFragment.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -15,6 +16,10 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringSystem;
+import com.facebook.rebound.SpringUtil;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
@@ -109,6 +114,8 @@ public class AddActionDialogFragment extends DialogFragment {
         gvActions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startClickAnimation(view);
+
                 type = (ActionType) gvActions.getItemAtPosition(position);
                 Activity activity = getActivity();
                 Intent intent = new Intent();
@@ -165,6 +172,32 @@ public class AddActionDialogFragment extends DialogFragment {
                 }
             }
         });
+    }
+
+    private void startClickAnimation(final View view) {
+        boolean isReboundEnabled = view.getTag(R.string.is_rebound_enabled_tag_key) != null;
+        if (!isReboundEnabled) {
+            final Spring spring = SpringSystem.create().createSpring();
+            spring.addListener(new SimpleSpringListener() {
+                @Override
+                public void onSpringUpdate(Spring spring) {
+                    float mappedValue = (float) SpringUtil.mapValueFromRangeToRange(
+                            spring.getCurrentValue(), 0, 1, 1, 0.5);
+                    view.setScaleX(mappedValue);
+                    view.setScaleY(mappedValue);
+                }
+            });
+            view.setTag(R.string.is_rebound_enabled_tag_key, spring);
+        }
+        final Spring spring = (Spring) view.getTag(R.string.is_rebound_enabled_tag_key);
+        spring.setEndValue(1);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                spring.setEndValue(0);
+            }
+        }, 100);
     }
 
     @Override
