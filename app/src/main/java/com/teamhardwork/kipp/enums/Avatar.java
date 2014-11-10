58de3d49ed133277e89db8e8cb4f951ec301d2d1
Kevin Leong <kgleong@gmail.com>
2014-11-10 07:44:21 -0800

    Update avatars.

--- app/src/main/java/com/teamhardwork/kipp/enums/Avatar.java
+++ app/src/main/java/com/teamhardwork/kipp/enums/Avatar.java
@@ -3,6 +3,25 @@ package com.teamhardwork.kipp.enums;
 import com.teamhardwork.kipp.R;
 
 public enum Avatar {
+    // Animal characters
+    CHESHIRE(R.drawable.avatar_cheshire),
+    GARFIELD(R.drawable.avatar_garfield),
+    GOOFY(R.drawable.avatar_goofy),
+    HOBBES(R.drawable.avatar_hobbes),
+    KERMIT(R.drawable.avatar_kermit),
+    LEONARDO(R.drawable.avatar_leonardo),
+    MARLIN(R.drawable.avatar_marlin),
+    MOJO(R.drawable.avatar_mojo),
+    NED(R.drawable.avatar_ned),
+    PIKACHU(R.drawable.avatar_pikachu),
+    REN(R.drawable.avatar_ren),
+    SCOOBY(R.drawable.avatar_scooby),
+    STIMPY(R.drawable.avatar_stimpy),
+    TAZMANIAN(R.drawable.avatar_tazmanian),
+    TIMON(R.drawable.avatar_timon),
+    WINNIE(R.drawable.avatar_winnie),
+
+    // Movie stars
     AMELIE(R.drawable.img_amelie),
     AVATAR(R.drawable.img_avatar),
     BLEACH(R.drawable.img_bleach),
